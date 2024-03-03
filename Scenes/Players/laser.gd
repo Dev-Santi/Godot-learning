cb8a9e9c1cc4a41c413e36963e92fb189a68f336
dev-santi <bauzalarrosasantiago@gmail.com>
2024-03-02 23:38:44 -0300

    i just watching from now i dont care

--- Scenes/Players/laser.gd
+++ Scenes/Players/laser.gd
@@ -3,5 +3,16 @@ extends Area2D
 var speed:int = 800
 var direction: Vector2 = Vector2.UP
 
+func _ready():
+	$Timer.start()
+
 func _process(delta):
 	position += direction * speed * delta
+
+func _on_body_entered(body):
+	if(body.has_method("hit")):
+		body.hit()
+	queue_free()
+
+func _on_timer_timeout():
+	queue_free()
--- Scenes/levels/level.gd
+++ Scenes/levels/level.gd
@@ -3,27 +3,25 @@ extends Node2D
 var laser_scene: PackedScene = preload("res://Scenes/Players/laser.tscn")
 var grenade_scene: PackedScene = preload("res://Scenes/Players/grenade.tscn")
 
-func _ready():
-	pass
-
-func _process(_delta):
-	pass
-
-func _on_gate_player_entered_gate(_body):
-	pass
-
 func _on_player_shooting(start_position, direction):
 	var new_laser = laser_scene.instantiate() as Area2D
 	new_laser.position = start_position
 	new_laser.rotation = direction.angle()
 	new_laser.direction = direction
-	
 	$Proyectiles.add_child(new_laser)
 
 func _on_player_throwing_grenade(pos,direction):
 	var new_grenade = grenade_scene.instantiate() as RigidBody2D
 	new_grenade.position = pos
 	new_grenade.linear_velocity = direction * new_grenade.speed
-	
-	
 	$Proyectiles.add_child(new_grenade)
+
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 2)
+
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 2)
